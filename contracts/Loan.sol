00b4468601a78cbf3ad463dc1b5262952c909666
vinayakhiwse <vinayak@empiricinfotech.com>
2024-07-05 18:05:21 +0530

    borrow amount from lender and lender pay money backto lender

--- contracts/Loan.sol
+++ contracts/Loan.sol
@@ -22,9 +22,11 @@ contract Loan {
     struct Borrower {
         address borrowerAddress;
         uint256 borrowingAmount;
+        uint256 totalPaidAmount;
     }
 
     struct CurrentTransaction {
+        address poolAddress;
         address lenderAddress;
         uint256 lendingAmount;
         uint256 lendingPercentage;
@@ -32,8 +34,9 @@ contract Loan {
         uint256 borrowingAmount;
     }
 
-    uint8 poolFee = 2;
+    uint256 poolFee = 2;
     uint256 minimumEth = 5;
+    uint256 totalContractCommision;
     mapping(address => Lender) lenderDetails;
     mapping(address => address[]) borrowersOfLenders;
     address[] allLenders;
@@ -84,123 +87,216 @@ contract Loan {
 
     function lendingLoan(
         uint256 _lendingAmount,
-        uint256 _lendingPercentage // payable
-    ) public // userMustLoggedIn
+        uint256 _lendingPercentage
+    ) public payable // userMustLoggedIn
     {
-        require(_lendingAmount >= minimumEth, "Lending Amount must be 10 Eth.");
-        // require(msg.value >= minimumEth, "You have to lend minimum 10 Eth.");
-        // require(
-        //     msg.value == (_lendingAmount * 1 ether),
-        //     "Amount must be equal to LendingAmount."
-        // );
+        require(
+            _lendingAmount >= minimumEth,
+            "Lending Amount must be minimum Eth."
+        );
+        require(msg.value >= minimumEth, "You have to lend minimum Eth.");
+        require(
+            msg.value == _lendingAmount,
+            "Amount must be equal to LendingAmount."
+        );
         Lender memory lender = Lender({
             lenderAddress: msg.sender,
-            lendingAmount: _lendingAmount,
+            lendingAmount: msg.value,
             lendingPercentage: _lendingPercentage,
-            remainingAmount: 0
+            remainingAmount: msg.value
         });
         lenderDetails[msg.sender] = lender;
         allLenders.push(msg.sender);
-        CurrentTransaction memory currentTransaction = CurrentTransaction({
-            lenderAddress: msg.sender,
-            lendingAmount: _lendingAmount,
-            lendingPercentage: _lendingPercentage,
-            borrowerAddress: address(0),
-            borrowingAmount: 0
-        });
-        currentTransactions[msg.sender][address(this)] = currentTransaction;
+        // CurrentTransaction memory currentTransaction = CurrentTransaction({
+        //     lenderAddress: msg.sender,
+        //     lendingAmount: _lendingAmount,
+        //     lendingPercentage: _lendingPercentage,
+        //     borrowerAddress: address(0),
+        //     borrowingAmount: 0
+        // });
+        // currentTransactions[msg.sender][address(this)] = currentTransaction;
     }
 
     function borrowingLoan(
         uint256 _borrowingAmount /*userMustLoggedIn*/
-    ) public {
+    ) public payable {
         require(
             _borrowingAmount >= minimumEth,
-            "Borrowing Amount must be 10 Eth."
+            "Borrowing Amount must be minimum Eth."
         );
-        // require(
-        //     address(this).balance >= _borrowingAmount,
-        //     "Bank has Insufficiant balance."
-        // );
-        Borrower memory borrower = Borrower({
-            borrowerAddress: msg.sender,
-            borrowingAmount: _borrowingAmount
-        });
-        borrowerDetails[msg.sender] = borrower;
-        allBorrowers.push(msg.sender);
+        require(
+            address(this).balance >= _borrowingAmount,
+            "Bank has Insufficiant balance."
+        );
 
         uint256 lenderLength = allLenders.length;
         for (uint256 i = 0; i < lenderLength; i++) {
-            for (uint256 j = 0; j < lenderLength - i - 1; j++) {
+            for (uint256 j = i + 1; j < lenderLength; j++) {
                 if (
-                    currentTransactions[allLenders[j]][address(this)]
-                        .lendingPercentage >=
-                    currentTransactions[allLenders[j + 1]][address(this)]
-                        .lendingPercentage
+                    lenderDetails[allLenders[i]].lendingPercentage >=
+                    lenderDetails[allLenders[j]].lendingPercentage
                 ) {
-                    tempTransactions[allLenders[j]][
-                        address(this)
-                    ] = currentTransactions[allLenders[j]][address(this)];
-                    currentTransactions[allLenders[j]][
-                        address(this)
-                    ] = currentTransactions[allLenders[j + 1]][address(this)];
-                    currentTransactions[allLenders[j + 1]][
-                        address(this)
-                    ] = tempTransactions[allLenders[j]][address(this)];
+                    address temp = allLenders[i];
+                    allLenders[i] = allLenders[j];
+                    allLenders[j] = temp;
                 }
             }
         }
 
-        // address lenderAddress;
-        CurrentTransaction storage currentTransaction;
         uint256 remainingAmount = 0;
         uint256 multipleRemainAmount = _borrowingAmount;
+        uint256 totalAmountValue;
+        uint256 payLoanToLender;
+
+        // uint256 calculateContractCommission;
+
         for (uint256 i = 0; i < allLenders.length; i++) {
             Lender storage lender = lenderDetails[allLenders[i]];
-            currentTransaction = currentTransactions[allLenders[i]][
-                address(this)
-            ];
-            currentTransaction.borrowerAddress = msg.sender;
-            if (
-                currentTransactions[allLenders[i]][address(this)]
-                    .lendingAmount >= multipleRemainAmount
-            ) {
-                remainingAmount =
-                    currentTransactions[allLenders[i]][address(this)]
-                        .lendingAmount -
-                    multipleRemainAmount;
-
-                currentTransaction.borrowingAmount =
-                    remainingAmount +
-                    currentTransactions[allLenders[i]][address(this)]
-                        .lendingAmount;
+            if (lender.remainingAmount >= multipleRemainAmount) {
+                remainingAmount = lender.remainingAmount - multipleRemainAmount;
                 lender.remainingAmount = remainingAmount;
-                console.log(remainingAmount);
-                break;
+                CurrentTransaction
+                    memory currentTransaction = CurrentTransaction({
+                        poolAddress: address(this),
+                        lenderAddress: lender.lenderAddress,
+                        lendingAmount: multipleRemainAmount,
+                        lendingPercentage: lender.lendingPercentage,
+                        borrowerAddress: msg.sender,
+                        borrowingAmount: multipleRemainAmount
+                    });
+                currentTransactions[allLenders[i]][
+                    msg.sender
+                ] = currentTransaction;
+
+                payLoanToLender =
+                    (currentTransaction.lendingAmount * 10 ** 18) +
+                    ((currentTransaction.lendingAmount *
+                        (currentTransaction.lendingPercentage * 10 ** 18)) /
+                        100);
+
+                // calculateContractCommission = (payLoanToLender * poolFee) / 100;
+                // totalContractCommision += calculateContractCommission;
+                // payLoanToLender -= calculateContractCommission;
+                totalAmountValue += payLoanToLender;
+
+                borrowersOfLenders[lender.lenderAddress].push(msg.sender);
+                lendersOfBorrowers[msg.sender].push(lender.lenderAddress);
+                remainingAmount = 0;
+                if (remainingAmount == 0) {
+                    break;
+                }
             } else {
                 multipleRemainAmount =
                     multipleRemainAmount -
-                    currentTransactions[allLenders[i]][address(this)]
-                        .lendingAmount;
-                currentTransaction.borrowingAmount = currentTransactions[
-                    allLenders[i]
-                ][address(this)].lendingAmount;
+                    lender.remainingAmount;
+                CurrentTransaction
+                    memory currentTransaction = CurrentTransaction({
+                        poolAddress: address(this),
+                        lenderAddress: lender.lenderAddress,
+                        lendingAmount: lender.lendingAmount,
+                        lendingPercentage: lender.lendingPercentage,
+                        borrowerAddress: msg.sender,
+                        borrowingAmount: lender.lendingAmount
+                    });
+                currentTransactions[allLenders[i]][
+                    msg.sender
+                ] = currentTransaction;
+
+                payLoanToLender =
+                    (currentTransaction.lendingAmount * 10 ** 18) +
+                    ((currentTransaction.lendingAmount *
+                        (currentTransaction.lendingPercentage * 10 ** 18)) /
+                        100);
+
+                // calculateContractCommission = (payLoanToLender * poolFee) / 100;
+                // totalContractCommision += calculateContractCommission;
+                // payLoanToLender -= calculateContractCommission;
+                totalAmountValue += payLoanToLender;
+
+                borrowersOfLenders[lender.lenderAddress].push(msg.sender);
+                lendersOfBorrowers[msg.sender].push(lender.lenderAddress);
+                lender.remainingAmount = 0;
             }
         }
 
-        // CurrentTransaction storage currentTransaction = currentTransactions[
-        //     lenderAddress
-        // ][address(this)];
-        // currentTransaction.borrowerAddress = msg.sender;
-        // currentTransaction.borrowingAmount = _borrowingAmount;
+        Borrower memory borrower = Borrower({
+            borrowerAddress: msg.sender,
+            borrowingAmount: _borrowingAmount,
+            totalPaidAmount: totalAmountValue
+        });
+        borrowerDetails[msg.sender] = borrower;
+        allBorrowers.push(msg.sender);
 
-        // borrowersOfLenders[lenderAddress].push(msg.sender);
-        // lendersOfBorrowers[msg.sender].push(lenderAddress);
+        (bool success, ) = payable(msg.sender).call{value: _borrowingAmount}(
+            ""
+        );
+        if (!success) {
+            revert Loan__TransferFailed();
+        }
+    }
 
-        // (bool success, ) = payable(msg.sender).call{
-        //     value: _borrowingAmount * 1 ether
+    function calculateEther() public view {
+        uint256 payLoanToLender;
+        payLoanToLender = (10 * 10 ** 18) + ((10 * (10 * 10 ** 18)) / 100);
+        console.log("payLoanToLender : ", payLoanToLender);
+
+        uint256 payLoanToLender2;
+        payLoanToLender2 = (20 * 10 ** 18) + ((20 * (3 * 10 ** 18)) / 100);
+        console.log("payLoanToLender2 : ", payLoanToLender2);
+
+        uint256 contractInterest1 = (payLoanToLender * poolFee) / 100;
+        console.log("contractInterest : ", contractInterest1);
+
+        uint256 contractInterest2 = (payLoanToLender2 * poolFee) / 100;
+        console.log("contractInterest : ", contractInterest2);
+
+        uint256 calculateContractCommision = contractInterest1 +
+            contractInterest2;
+        console.log(
+            "calculateContractCommision : ",
+            calculateContractCommision
+        );
+
+        console.log(payLoanToLender + payLoanToLender2);
+    }
+
+    function borrowerPayLoan()
+        public
+        payable
+        /* userMustLoggedIn*/
+        onlyBorrower
+    {
+        require(
+            msg.value >=
+                (borrowerDetails[msg.sender].totalPaidAmount / 10 ** 18),
+            "Pay atleast Your Borrowing Amount"
+        );
+        uint256 borrowerLoanAmount = msg.value;
+        uint256 payLoanToLender;
+
+        uint256 lenderLength = allLenders.length;
+        for (uint256 i = 0; i < lenderLength; i++) {
+            CurrentTransaction storage currentTransaction = currentTransactions[
+                allLenders[i]
+            ][msg.sender];
+
+            payLoanToLender = currentTransaction.lendingAmount;
+            if (payLoanToLender <= borrowerLoanAmount) {
+                borrowerLoanAmount -= payLoanToLender;
+                lenderDetails[allLenders[i]].remainingAmount += payLoanToLender;
+                (bool success, ) = payable(allLenders[i]).call{
+                    value: payLoanToLender
+                }("");
+                if (!success) {
+                    revert Loan__TransferFailed();
+                }
+            }
+        }
+
+        // (bool successFee, ) = payable(address(this)).call{
+        //     value: totalContractCommision / 10**17
         // }("");
-        // if (!success) {
+        // if (!successFee) {
         //     revert Loan__TransferFailed();
         // }
     }
@@ -232,7 +328,10 @@ contract Loan {
         return borrowersOfLenders[msg.sender];
     }
 
-    function getCurrentTransaction()
+    function getCurrentTransaction(
+        address lender,
+        address borrower
+    )
         public
         view
         returns (
@@ -244,8 +343,8 @@ contract Loan {
         )
     {
         CurrentTransaction memory currentTransaction = currentTransactions[
-            msg.sender
-        ][address(this)];
+            lender
+        ][borrower];
         return (
             currentTransaction.lenderAddress,
             currentTransaction.lendingAmount,
